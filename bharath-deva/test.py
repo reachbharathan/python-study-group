0d7cdaf4264d9547fa6a2f4a5b9e1c1ee9eb7912
Bharathan Kumaran <bharathk@thoughtworks.com>
2020-04-16 20:06:41 +0800

    Update test.py to execute without error - BharathanKumaran

--- bharath-deva/test.py
+++ bharath-deva/test.py
@@ -5,8 +5,6 @@ class Test(unittest.TestCase):
     def test(self):
         result = dummy.Sumofthenumber(123)
         self.assertEqual(result,6)
-        #self.assertEqual(result,7)
-    try:
-         unittest.main()
-    except SystemExit:
-        pass
+        self.assertEqual(result,7)
+
+unittest.main()
