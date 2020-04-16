aa9c7046034b66806795ff9c5775c4fa464b3b6d
Bharathan Kumaran <bharathk@thoughtworks.com>
2020-04-16 20:27:07 +0800

    update the test to validate the error scenarios - Bharathan

--- bharath-deva/test.py
+++ bharath-deva/test.py
@@ -2,9 +2,12 @@ import unittest
 import dummy
 
 class Test(unittest.TestCase):
-    def test(self):
+    def test_positive(self):
         result = dummy.Sumofthenumber(123)
         self.assertEqual(result,6)
-        self.assertEqual(result,7)
+    
+    def test_negative(self):
+        result = dummy.Sumofthenumber("abc")
+        self.assertEqual(isinstance(result,ValueError),True)
 
 unittest.main()
